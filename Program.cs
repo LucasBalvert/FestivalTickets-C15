2cc2dca82bf6eeab492851b493cd793299ba10e8
Lucas <lucas@balvert.net>
2025-04-10 20:21:16 +0200

    commentaar aangepast voor program.cs

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ namespace FestivalTickets_C15
     internal static class Program
     {
         /// <summary>
-        /// The main entry point for the application.
+        /// The main entry point for the application.FestivalTickets
         /// </summary>
         [STAThread]
         static void Main()
